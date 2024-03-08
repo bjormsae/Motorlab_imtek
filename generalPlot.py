75afd832c1bc167022f93d0a7d5732f4eb9fd8a3
Bjørn Magnus Sætrom <bjorn.magnus.satrom@gmail.com>
2024-03-08 11:34:05 +0100

    added plots

--- generalPlot.py
+++ generalPlot.py
@@ -6,9 +6,9 @@ x = []
 channel_one = []
 channel_two = []
 
-filename = 'squareWave_04hz'
-filepath = 'motorlabData/' + filename + '.csv'
-newfilePath = 'plots/' + filename + '.png'
+filename1 = 'H_P_r'
+filepath = 'motorlabData/' + filename1 + '.csv'
+newfilePath = 'plots/' + filename1 + '.png'
 
 with open(filepath, 'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
@@ -16,16 +16,25 @@ with open(filepath, 'r') as csvfile:
     for row in plots:
         x.append(float(row[0]))
         channel_one.append(float(row[1]))
-        channel_two.append(float(row[2]))
+
+filename2 = 'H_P_m'
+filepath = 'motorlabData/' + filename2 + '.csv'
+
+
+with open(filepath, 'r') as csvfile2:
+    plots2 = csv.reader(csvfile2, delimiter=',')
+    for row in plots2:
+        channel_two.append(float(row[1]))
 
 x = np.array(x)
 x = x - x[0]
 
 print(len(x))
 
-
-plt.plot(x, channel_one, label='CH1')
-plt.plot(x, channel_two, label='CH2')
+j = 12500
+k = 7500
+plt.plot(x[:j-k], channel_one[k:j], label='CH1')
+plt.plot(x[:j-k], channel_two[k:j], label='CH2')
 plt.xlabel("$t$ [s]")
 plt.ylabel("$V$ [V]")
 plt.legend(loc=2)
